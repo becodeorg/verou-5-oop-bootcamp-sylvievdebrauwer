9fee0711a07102088943455531d1254164ce2bd4
sylvievdebrauwer <sylvie.debrauwer@gmail.com>
2024-01-10 16:52:45 +0100

    case 4 basics

---
+++ use_case4/Student.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+class Student 
+{
+    // private properties name and grade
+    // create constructor to initialize name and grade
+    // create methods to get name and grade
+}
