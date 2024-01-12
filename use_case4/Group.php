debb667876c84e9664977851c23e00bac2b5c54b
sylvievdebrauwer <sylvie.debrauwer@gmail.com>
2024-01-12 12:06:12 +0100

    Finished All Cases

--- use_case4/Group.php
+++ use_case4/Group.php
@@ -1,1 +1,44 @@
 <?php
+require_once("Student.php");
+class Group 
+{
+    private array $students;
+
+    public function __construct()
+    {
+        $this->students = [];
+    }
+
+    public function getStudents()
+    {
+        return $this->students;
+    }
+
+    public function addStudent(Student $student)
+    {
+        $this->students[] = $student;
+    }
+
+    public function changeGroup(Group $group, Student $student)
+    {
+        $group->addStudent($student);
+        $index = array_search($student, $this->students);
+
+        if ($index !== false) {
+            unset($this->students[$index]);
+        }
+    }
+
+    public function getAverageGrade()
+    {
+        $average = 0;
+
+        foreach ($this->students as $student) {
+            if ($student instanceof Student) $average += $student->getGrade();
+        }
+
+        $average = $average / count($this->students);
+
+        return $average;
+    }
+}
